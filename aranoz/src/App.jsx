f082b587ef4853a23b1019db8502cf1ae4b0604a
proffdoc <mi7layp4g@code.edu.az>
2024-05-21 11:06:33 +0400

    front little added

--- aranoz/src/App.jsx
+++ aranoz/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './layouts/Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Pages from './pages/Page'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Errorpage from './pages/Errorpage'
+import Footer from './layouts/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-  s
+    <>
+    <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/shop" element={<Shop/>}/>
+    <Route path="/pages" element={<Pages/>}/>
+    <Route path="/blog" element={<Blog/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    <Route path="/contact" element={<Errorpage/>}/>
+  </Routes>
+  <Footer/>
+  </>
   )
 }
 
---
+++ aranoz/src/layouts/Header/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./index.scss";
+import { NavLink } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
+import { CiHeart } from "react-icons/ci";
+import { LuShoppingCart } from "react-icons/lu";
+const Header = () => {
+  return (
+    <div>
+        <div className="container">
+            <div id="header-left">
+              <img src="https://preview.colorlib.com/theme/aranoz/img/logo.png.webp" alt="aranoz." />
+            </div>
+            <div id="header-middle">
+            <ul>
+                <NavLink to={"/"}><li>Home</li></NavLink>
+                <NavLink to={"/shop"}><li>Shop</li></NavLink>
+                <NavLink to={"/pages"}><li>Pages</li></NavLink>
+                <NavLink to={"/blog"}><li>Blog</li></NavLink>
+                <NavLink to={"/contact"}><li>Contact</li></NavLink>
+            </ul>
+            </div>
+            <div id="header-right">
+              <div className="icons">
+              <IoIosSearch />
+              <CiHeart />
+              <LuShoppingCart />
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
